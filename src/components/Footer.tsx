e2cd4cafce4957a6a8580110381745482a79d3ab
Fubon <thefubon@gmail.com>
2025-04-29 16:39:04 +0300

    Измения в Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-muted mt-12 py-6">
       <div className="max-w-7xl mx-auto px-4 text-center text-foreground">
-        &copy; {new Date().getFullYear()} MDX Блог на Next.js 15
+        &copy; 2004-{new Date().getFullYear()} Fubon.
       </div>
     </footer>
   )
