ebc8b452cba5b8fafef45da3f1b8a3edc35fb861
Fubon <thefubon@gmail.com>
2025-04-29 16:35:05 +0300

    Update Audion Player

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,5 +1,5 @@
 import AudioPlayer from '@/components/AudioPlayer'
 
 export default function MusicPage() {
-  return  <div className="container-fluid"><AudioPlayer /></div>
+  return  <div className="container-fluid flex justify-center"><AudioPlayer /></div>
 }
