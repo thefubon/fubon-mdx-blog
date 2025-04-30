6378612d13eba4ac3a06873e2a60a220d315a44b
Fubon <thefubon@gmail.com>
2025-04-30 10:27:49 +0300

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,8 @@
 // src/components/Header.tsx
 import { Link } from 'next-view-transitions'
 import { Button } from './ui/button'
-import Navbar from './Navbar'
 import Logo from './Logo'
-import { FileMusic, LayoutDashboard, Rabbit } from 'lucide-react'
 import Dropdown from './Dropdown'
-import { menuItems } from '@/data/navbar'
-
-// Карта иконок
-const iconComponents = {
-  'Rabbit': <Rabbit />,
-  'LayoutDashboard': <LayoutDashboard />,
-  'FileMusic': <FileMusic />
-}
-
-// Преобразование данных из menuItems в формат, необходимый для Navbar
-const navItems = menuItems.map(item => ({
-  label: item.name,
-  href: item.link,
-  icon: item.iconName ? iconComponents[item.iconName as keyof typeof iconComponents] : undefined
-}))
 
 export default function Header() {
   return (
@@ -28,10 +11,6 @@ export default function Header() {
         <Link href="/">
           <Logo />
         </Link>
-      </div>
-
-      <div className="hidden md:inline-block">
-        <Navbar navLinks={navItems} />
       </div>
 
       <div className="header__end">
--- src/data/navbar.ts
+++ src/data/navbar.ts
@@ -1,12 +1,10 @@
-// src/data/navbar.ts (без JSX)
 export type MenuItem = {
   name: string
   link: string
-  iconName?: string // Используем строковые идентификаторы вместо React компонентов
 }
 
 export const menuItems: MenuItem[] = [
-  { name: 'Работа', link: '/work', iconName: 'Rabbit' },
-  { name: 'Блог', link: '/blog', iconName: 'LayoutDashboard' },
-  { name: 'Музыка', link: '/music', iconName: 'FileMusic' },
+  { name: 'Работа', link: '/work' },
+  { name: 'Блог', link: '/blog' },
+  { name: 'Музыка', link: '/music' },
 ]
