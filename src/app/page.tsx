552f40d6f199d24453d3dc5c58efcc09ac0cd5b2
Fubon <thefubon@gmail.com>
2025-05-01 10:33:31 +0300

    upd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           </div>
 
           <h1 className="text-h1 font-extrabold mb-4">
-            Добро пожаловать в MDX Блог
+            Добро пожаловать!
           </h1>
 
           <p className="description text-gray-600 mb-8 max-w-3xl mx-auto">
