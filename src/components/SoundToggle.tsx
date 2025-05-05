55010b03d2f810eb3444284a9f5fb9c9d3ace984
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-05-05 20:01:44 +0300

    upd

--- src/components/SoundToggle.tsx
+++ src/components/SoundToggle.tsx
@@ -3,28 +3,23 @@
 
 import { useSoundContext } from '@/contexts/SoundProvider'
 import { Volume2, VolumeOff } from 'lucide-react'
+import { Button } from './ui/button'
 
 export function SoundToggle() {
   const { enabled, setEnabled } = useSoundContext()
 
   return (
     <>
-      <button
+      <Button
         onClick={() => setEnabled(!enabled)}
         className="size-12 md:size-[var(--button-height)] bg-foreground hover:bg-fubon-primary text-background rounded-full relative z-50 cursor-pointer flex justify-center items-center transition-colors duration-300"
         aria-label={enabled ? 'Выключить звуки' : 'Включить звуки'}>
         {enabled ? (
-          <Volume2
-            size={24}
-            strokeWidth={1.8}
-          />
+          <Volume2 className="size-6 stroke-[1.8]" />
         ) : (
-          <VolumeOff
-            size={24}
-            strokeWidth={1.8}
-          />
+          <VolumeOff className="size-6 stroke-[1.8]" />
         )}
-      </button>
+      </Button>
     </>
   )
 }
