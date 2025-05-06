0d15274c99549d03446335779095f228bb1292ac
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2025-05-06 13:32:57 +0300

    Установил - Velustro

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,14 +1,26 @@
 import PageWrapper from '@/components/PageWrapper'
-import React from 'react'
+import Container from '@/components/ui/Container'
+import { Velustro } from 'uvcanvas'
 
 export default function WorkPage() {
   return (
     <PageWrapper
       title="Работа"
       description="Описание страницы или какой-то вводный текст.">
+      <div className="w-full min-h-screen">
+        <Container
+          padding
+          space
+          className="relative">
+          <div className="px-[var(--padding-x)] absolute top-4 left-0 z-10 text-background">
+            <h2 >Работы</h2>
+          </div>
+        </Container>
 
-      <div className="container-fluid">Работы временно тут</div>
-
+        <div className="!w-full !min-h-screen">
+          <Velustro className="!w-full !min-h-screen relative" />
+        </div>
+      </div>
     </PageWrapper>
   )
 }
